cdc825d7223ba7e0180759698b7c95358836ac40
Shuaib6869 <Suvebansari820@gmail.com>
2024-12-28 23:46:54 -0800

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/saimvampire34/ROSEXMSUIC",
+    "https://github.com/Shuaib6869/ROSEXMSUIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
